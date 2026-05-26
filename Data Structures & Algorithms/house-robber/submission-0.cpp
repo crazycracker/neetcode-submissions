d26427c620b48d5185c54ff5c2b01427f8717f88
Vinay Raj Manchala <vinayraj33@live.com>
2026-05-26 22:50:17 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> memo;
+
+    int dfs(vector<int>& nums, int i) {
+        if(i >= nums.size()) return 0;
+        if(memo[i] != -1) return memo[i];
+        memo[i] = max(dfs(nums, i + 1), nums[i] + dfs(nums, i + 2));
+        return memo[i];
+    }
+    int rob(vector<int>& nums) {
+        memo.resize(nums.size(), -1);
+        return dfs(nums, 0);
+    }
+};
